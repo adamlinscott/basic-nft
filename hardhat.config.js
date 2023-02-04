0139ae33e44e1bed37e577d287a5bb69d902360c
Adam Linscott <adamrlinscott@gmail.com>
2023-02-04 12:12:55 +0000

    updated chains for deployment

--- hardhat.config.js
+++ hardhat.config.js
@@ -11,8 +11,7 @@ require("dotenv").config();
  * @type import('hardhat/config').HardhatUserConfig
  */
 const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL
-const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL
-const ROPSTEN_RPC_URL = process.env.ROPSTEN_RPC_URL
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
 const PRIVATE_KEY = process.env.PRIVATE_KEY
 const REPORT_GAS = process.env.REPORT_GAS || false
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
@@ -24,16 +23,9 @@ module.exports = {
       chainId: 31337,
       blockConfirmations: 1
     },
-    rinkeby: {
-      url: RINKEBY_RPC_URL || "",
-      chainId: 4,
-      blockConfirmations: 6,
-      saveDeployments: true,
-      accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
-    },
-    ropsten: {
-      url: ROPSTEN_RPC_URL || "",
-      chainId: 3,
+    goerli: {
+      url: GOERLI_RPC_URL || "",
+      chainId: 5,
       blockConfirmations: 6,
       saveDeployments: true,
       accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
@@ -77,9 +69,8 @@ module.exports = {
   },
   etherscan: {
     apiKey: {
-      ropsten: ETHERSCAN_API_KEY,
       mainnet: ETHERSCAN_API_KEY,
-      rinkeby: ETHERSCAN_API_KEY,
+      goerli: ETHERSCAN_API_KEY,
     }
   },
   gasReporter: {
